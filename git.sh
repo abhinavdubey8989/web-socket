db3a1ea2e6309672bb8f2ffed8a0c3d756182c6c
abhinav-dubey-hs <abhinav.dubey@helpshift.com>
2024-01-01 12:25:09 +0530

    'checking'

--- git.sh
+++ git.sh
@@ -4,6 +4,13 @@ echo "enter commit msg : "
 
 read commit_msg
 
+
+
 git add .
-git commit -m $commit_msg
+git commit -m \'$commit_msg\'
 git push
+
+
+# # git add .
+# echo git commit -m "$commit_msg"
+# # git push
