9591c152f0a62296132e383874eda9bb8c87c5af
abhinav-dubey-hs <abhinav.dubey@helpshift.com>
2024-01-02 23:17:32 +0530

    'import-fixes'

--- socket-project-02-scaling-using-redis/src/manager/sendMsgToClient.ts
+++ socket-project-02-scaling-using-redis/src/manager/sendMsgToClient.ts
@@ -1,8 +1,9 @@
+import { Server as SockerServer } from "socket.io";
 
 
 
 
 // used to check modularity only
-export function sendMsg(io, data){
+export function sendMsg(io : SockerServer, data){
     io.emit('newMessageToClients' , {fromServer : data.fromClient})
 }
